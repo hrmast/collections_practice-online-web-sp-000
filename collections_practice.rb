78236388c984f48a1b724c88c65c0fff9433adfa
Heath Mast <hrmast@gmail.com>
2020-04-29 00:04:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,7 @@ def swap_elements(array)
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) #["blake", "scott", "ashley"]
+swap_elements(["blake", "ashley", "scott"])
 
 def reverse_array(array)
   array.reverse
@@ -29,4 +29,8 @@ end
 
 def find_a(array)
   array.find_all {|word| word[0] == "a"}
+end
+
+def sum_array(array)
+
 end
